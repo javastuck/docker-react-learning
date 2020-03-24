8b5909e68a40fcb5e4ceadf6dd15205171b97ab3
Justin Stuck <justin.stuck@gmail.com>
2020-03-23 19:22:42 -0500

    removing learn react link

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
         <p>
           What's up, chicken snoots? Want to play Animal Crossing?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
